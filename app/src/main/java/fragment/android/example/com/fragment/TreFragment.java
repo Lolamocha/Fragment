56f81fb7be1623f6cc47fb818257f73052301ca8
Lolamocha <manola.quinti@gmail.com>
2018-09-29 12:32:14 +0200

    aggiungo Activity

---
+++ app/src/main/java/fragment/android/example/com/fragment/TreFragment.java
@@ -0,0 +1,13 @@
+package fragment.android.example.com.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TreFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tre_fragment);
+    }
+}
