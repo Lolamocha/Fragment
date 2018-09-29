4ddfca778bfd8869696c533ba5335f2605c8f193
Lolamocha <manola.quinti@gmail.com>
2018-09-29 12:21:44 +0200

    Initial commit

---
+++ app/src/main/java/fragment/android/example/com/fragment/UnoFragment.java
@@ -0,0 +1,13 @@
+package fragment.android.example.com.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UnoFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uno_fragment);
+    }
+}
